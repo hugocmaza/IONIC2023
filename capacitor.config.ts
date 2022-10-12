7a53b9e33ed62297ef9e068cb68f43e386ebc6f0
hmazaITP <hmaza@itpatagonia.com>
2022-10-12 19:35:42 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'IONIC2023',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
